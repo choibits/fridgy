5c9469489d5d0bd7323de5a8728035a671887a3a
achoi <achoi@indeed.com>
2025-07-29 09:27:11 -0700

    setup database in application.properties file

--- src/main/java/com/fridgy/app/FridgyApplication.java
+++ src/main/java/com/fridgy/app/FridgyApplication.java
@@ -2,12 +2,15 @@ package com.fridgy.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FridgyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FridgyApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(FridgyApplication.class, args);
+        String currentAppName = context.getEnvironment().getProperty("spring.application.name");
+        System.out.println("Current application name: " + currentAppName);
     }
 
 }
