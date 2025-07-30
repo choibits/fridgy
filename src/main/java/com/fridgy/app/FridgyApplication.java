2a5bfe283007c90c337185f9b60e160fafff3ec4
achoi <achoi@indeed.com>
2025-07-29 20:28:04 -0700

    add predeploy code

--- src/main/java/com/fridgy/app/FridgyApplication.java
+++ src/main/java/com/fridgy/app/FridgyApplication.java
@@ -10,7 +10,7 @@ public class FridgyApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(FridgyApplication.class, args);
         String currentAppName = context.getEnvironment().getProperty("spring.application.name");
-        System.out.println("Current application name: " + currentAppName);
+        System.out.println("Current application name: ➡️" + currentAppName);
     }
 
 }
